5356f276366abc3427c4599d331eb8916ff39363
M47Hs <60658558+M47Hs@users.noreply.github.com>
2021-07-24 21:20:49 +0200

    bugfix holder

--- src/components/NewOwners.js
+++ src/components/NewOwners.js
@@ -45,7 +45,7 @@ function NewOwners({ proto, quality }) {
                     {trades.map((trade) => (
                         <tr>
 
-                            <td><Link to={`/godsunchained/protos/${trade.owner}`}>
+                            <td><Link to={`/godsunchained/user/${trade.owner}`}>
                                 {trade.owner}
                             </Link>
                             </td>
